3262023662d8519bb89a096da678c21461dea0d2
wanglinbo-z <53314175+wanglinbo-z@users.noreply.github.com>
2019-08-03 12:00:13 +0800

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,49 +1,56 @@
-﻿#include "pch.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	int e = 0;
-	int a = 0;
-	int c = 0;
-	int d[99999];
+		int e = 0;
+		long long a = 0;
+		int c = 0;
+		int d[32];
 
-	while (e <= 0)
-	{
-		cout << endl << "请输入需要转换的数字：";
-		cin >> a;
-		c = 0;
-
-		if (a == 0) 
+		while (e <= 0)
 		{
-			e = 1;
-		}
-
-		else
-		{
-			for (; a > 0;)
+			cout << endl << "请输入需要转换的数字：";
+			cin >> a;
+			c = 0;
+			if (a >= 9223372036854775808)
 			{
-				d[c] = a % 2;
-				a = a / 2;
-				c++;
+				cout << endl << "数据错误" << endl;
 			}
-
-			c--;
-			cout << "结果：";
-
-			while (c >= 0)
+			else
 			{
-				cout << d[c];
+				if (a == 0)
+				{
+					cout << "再见";
+					e = 1;
+				}
+
+				else
+				{
+					for (; a > 0;)
+					{
+						d[c] = a % 2;
+						a = a / 2;
+						c++;
+					}
+
+					c--;
+					cout << "结果：";
+
+					while (c >= 0)
+					{
+						cout << d[c];
+						c--;
+					}
+				}
+			}
+			while (c > 0)
+			{
+				d[c] = 0;
 				c--;
 			}
 		}
-	}
-	while (c > 0)
-	{
-		d[c] = 0;
-		c--;
-	}
-	return 0;
+		return 0;
+	
 }
